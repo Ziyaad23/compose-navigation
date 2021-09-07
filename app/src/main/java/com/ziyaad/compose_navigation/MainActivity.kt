ada4c3c5eed36939b36512e0d2e6a09d2677f5d3
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-07 22:31:25 +0400

    [+][ADD] Basic navigation in jetpack compose

--- app/src/main/java/com/ziyaad/compose_navigation/MainActivity.kt
+++ app/src/main/java/com/ziyaad/compose_navigation/MainActivity.kt
@@ -14,25 +14,13 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposenavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            Navigation()
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposenavigationTheme {
-        Greeting("Android")
-    }
+    Navigation()
 }
